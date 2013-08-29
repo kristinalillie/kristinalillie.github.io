f0737e8f34be3d60e15867577557fcfcad6ba7d3
Kristina Lillie <kristinaalillie@gmail.com>
2013-08-28 18:26:44 -0700

    Procfile

--- web.js
+++ web.js
@@ -1,1 +1,12 @@
-web.js
+var express = require("express"); 
+var app = express(); 
+app.use(express.logger()); 
+
+app.get('/', function(request, response)  {
+  response.send('Attempt #3    So creative')
+}); 
+
+var port = process.env.PORT || 5000; 
+app.listen(port, function() {
+  console.log("Listening on " + port); 
+}); 
